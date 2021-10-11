1a7d07b088e00213c585d4b5a86ded4ac6afb0d3
kimbosung <yhi002521@gmail.com>
2021-10-11 18:55:09 +0900

    SpringReactiveChatServer

---
+++ src/main/java/com/example/reactormongodb/ChatController.java
@@ -0,0 +1,36 @@
+package com.example.reactormongodb;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    @CrossOrigin
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMessage(@PathVariable String sender, @PathVariable String receiver) {
+        return chatRepository.findBySender(sender, receiver).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return chatRepository.findByRoomNum(roomNum).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @PostMapping("/chat")
+    public Mono<Chat> setMessage(@RequestBody Chat chat) {
+        chat.setCreateAt(LocalDateTime.now());
+        return chatRepository.save(chat);
+    }
+}
---
+++ src/main/java/com/example/reactormongodb/ChatRepository.java
@@ -0,0 +1,17 @@
+package com.example.reactormongodb;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+
+    @Tailable
+    @Query("{sender:?0, receiver:?1}")
+    Flux<Chat> findBySender(String sender, String receiver);
+
+    @Tailable
+    @Query("{roomNum: ?0}")
+    Flux<Chat> findByRoomNum(Integer roomNum);
+}
